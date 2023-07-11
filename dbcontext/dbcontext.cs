12f8ff2ba84fe903c5556d46c4f2800c241619fd
Michael Goboola <47690301+michaelgobz@users.noreply.github.com>
2023-07-11 14:43:03 +0000

    using microsoft entity framework

--- dbcontext/dbcontext.cs
+++ dbcontext/dbcontext.cs
@@ -1,12 +1,17 @@
 using Microsoft.EntityFrameworkCore;
-using WiredBrainApp.entities
+using WiredBrainApp.entities;
 namespace WiredBrainApp.Dbcontext
 {
 
     public class DbStorageContext : DbContext
     {
-        public <Employee> employees => Set<Employee>();
+        public DbSet<Employee> employees => Set<Employee>();
         public DbSet<Organisation> organisations => Set<Organisation>();
     }
+
+    public override onConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+		optionsBuilder.UseInMemoryDatabase("database");
+	}
 
 }
